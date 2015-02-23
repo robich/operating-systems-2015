01cdc27034f9761f5e3275af6462710008a793d5
root <root@oslab.epfl.ch>
2015-02-23 23:29:46 +0100

    Repair recompile script and add colors to info msgs

--- assignment01/scripts/full-kernel-compile.sh
+++ assignment01/scripts/full-kernel-compile.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+# Compiles and installs the linux kernel, then reboots
 
-cd /
-cd usr/src/linux
-echo "Compiling..."
-make -j2 bzImage
-echo "Copying..."
-sudo cp /usr/src/linux/arch/x86/boot/bzImage /boot/vmlinuz-3.18.3+
-echo "Making modules..."
-make modules
-echo "Installing modules..."
+
+cd ../linux
+echo -e "\e[32mCompiling...\e[0m"
+sudo make -j2 bzImage
+echo -e "\e[32mCopying...\e[0m"
+sudo cp arch/x86/boot/bzImage /boot/vmlinuz-3.18.3+
+echo -e "\e[32mMaking modules...\e[0m"
+sudo make modules
+echo -e "\e[32mInstalling modules...\e[0m"
 sudo make modules_install
-echo "Updating kernel..."
+echo -e "\e[32mUpdating kernel...\e[0m"
 sudo update-initramfs -k 3.18.3+ -u
-echo "Rebooting..."
+echo -e "\e[32mRebooting...\e[0m"
 sudo reboot
-echo "Welcome back!"
-exit 0
