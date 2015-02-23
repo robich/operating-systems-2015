8b5f758dd7f7a1d7e220cc2b8fcb8f757e95b251
Julien Perrenoud <julien.perrenoud@epfl.ch>
2015-02-23 20:45:03 +0100

    Update load_from_git.sh

--- assignment01/scripts/load_from_git.sh
+++ assignment01/scripts/load_from_git.sh
@@ -7,14 +7,14 @@ $dir=${PWD}
 cd /usr/git/operating-systems-2015
 
 # Pull from git
-echo -e "[\e[34mInfo\e[0m] Pulling from git..."
+echo -e "[\e[94mInfo\e[0m] Pulling from git..."
 git pull
 
 # Copy Kernel
-echo -e "[\e[34mInfo\e[0m] Copying Kernel Directory..."
+echo -e "[\e[94mInfo\e[0m] Copying Kernel Directory..."
 sudo cp -r /usr/git/operating-systems-2015/assignment01/linux/kernel/* /usr/src/linux/kernel
 # Copy Syscalls
-echo -e "[\e[34mInfo\e[0m] Copying Syscalls Directory..."
+echo -e "[\e[94mInfo\e[0m] Copying Syscalls Directory..."
 sudo cp -r /usr/git/operating-systems-2015/assignment01/linux/arch/x86/syscalls/* /usr/src/linux/arch/x86/syscalls
 
 # Return into initial directory
