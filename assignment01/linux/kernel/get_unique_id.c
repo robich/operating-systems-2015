07b9d53cd6ead93cea35849dcd093b9a018cee18
root <root@oslab.epfl.ch>
2015-02-25 14:05:03 +0100

    Copy include

--- assignment01/linux/kernel/get_unique_id.c
+++ assignment01/linux/kernel/get_unique_id.c
@@ -1,14 +1,37 @@
 #include<linux/linkage.h>
 #include<linux/uaccess.h>
+#include<linux/module.h>
+#include<linux/init.h>
 #include<linux/spinlock.h>
-asmlinkage long sys_get_unique_id(int *uuid)
+
+spinlock_t lock;
+long id;
+
+static int __init hi(void)
 {
-	// Hi guys! (from Robin, with <3)
-	// Deal with concurrency !
-	// HELLO_BITCHES
-	// Use put_user
-	int x = 10;
-	return put_user(x, uuid);
-	//Working here (Alain)
+	spin_lock_init(&lock);
+	id = 0;
+
+	return 0;
 }
 
+module_init(hi);
+
+asmlinkage long sys_get_unique_id(int *uuid)
+{
+	// Lock
+	spin_lock(&lock);
+
+	// Try to write the ID
+	long res;
+	res = put_user(id, uuid);
+	id++;
+
+	spin_unlock(&lock);
+
+	if(res == -EFAULT) {
+		printk(KERN_ERR "Trying to access an illegal memory location !\n");
+	}
+
+	return res;
+}
