481cfacf721c02633496eaada3243c2f6c9cba05
Robin Genolet <robin.genolet@epfl.ch>
2015-02-26 21:31:08 +0100

    Update get_unique_id.c

--- assignment01/linux/kernel/get_unique_id.c
+++ assignment01/linux/kernel/get_unique_id.c
@@ -25,12 +25,14 @@ asmlinkage long sys_get_unique_id(int *uuid)
 	// Try to write the ID
 	long res;
 	res = put_user(id, uuid);
-	id++;
 
 	spin_unlock(&lock);
 
 	if(res == -EFAULT) {
 		printk(KERN_ERR "Trying to access an illegal memory location !\n");
+	} else {
+		// Only increment uuid if call is valid
+		id++;
 	}
 
 	return res;
