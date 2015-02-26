1d52e65d9ae5520e8839d424a34a702965523da1
root <root@oslab.epfl.ch>
2015-02-26 21:47:14 +0100

    Code compile, not tested yet

--- assignment01/linux/kernel/get_child_pids.c
+++ assignment01/linux/kernel/get_child_pids.c
@@ -7,8 +7,6 @@
 #include<linux/uaccess.h>
 #include<linux/list.h>
 
-#define current (current_thread_info()->task)
-
 asmlinkage long sys_get_child_pids(pid_t* list, size_t limit, size_t* num_children)
 {
 	// return value of the function
@@ -18,12 +16,6 @@ asmlinkage long sys_get_child_pids(pid_t* list, size_t limit, size_t* num_childr
 	// Counter for children tasks
 	size_t children;
 	children = 0;
-
-	// Structures for current task and a list for its children
-	struct task_struct* current_task;
-	struct list_head* children_tasks_list;
-	current_task = current;
-	children_tasks = current_task->children;
 
 	// First check on memory validity
 	if (list == NULL && limit != 0) {
@@ -35,23 +27,25 @@ asmlinkage long sys_get_child_pids(pid_t* list, size_t limit, size_t* num_childr
 
 	// Iterate on children
 	struct task_struct *child;
-	list_for_each_entry(child, &children_tasks_list, *next) {
+	list_for_each_entry(child, &current->children, sibling) {
 
 		// Extract pid of child
 		pid_t child_id;
 		child_id = child->pid;
 
-		printk(KERN_DEBUG "Looking at child pid %ld" child_id);
+		printk(KERN_DEBUG "Looking at child pid %zu", child_id);
 
 		// Increment number of children
 		children = children + 1;
 
 		if (children <= limit) {
 			// Store children pid in the list
-			if(put_user(child_id, list[children-1]) == -EFAULT){
+			if(put_user(child_id, list) == -EFAULT){
 				printk(KERN_ERR "Error while writing children id");
 				return -EFAULT;
 			}
+			// Moves pointer to next memory slot
+			list++;
 		}
 	}
 
