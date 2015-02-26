921afb5277ba1d9f348fb9f7ff4748575994f2c2
root <root@oslab.epfl.ch>
2015-02-26 23:05:24 +0100

    fix checkpatck.pl issues

--- assignment01/linux/kernel/get_child_pids.c
+++ assignment01/linux/kernel/get_child_pids.c
@@ -7,57 +7,58 @@
 #include<linux/uaccess.h>
 #include<linux/list.h>
 
-asmlinkage long sys_get_child_pids(pid_t* list, size_t limit, size_t* num_children)
+asmlinkage long sys_get_child_pids(pid_t *list, size_t limit, size_t *num_children)
 {
-	// return value of the function
+	/* return value of the function */
 	long res;
+
 	res = 0;
 
-	// Counter for children tasks
+	/* Counter for children tasks */
 	size_t children;
+
 	children = 0;
 
-	// First check on memory validity
+	/* First check on memory validity */
 	if (list == NULL && limit != 0) {
 		printk(KERN_ERR "List is NULL whereas limit is nonzero!\n");
 		return -EFAULT;
 	}
 
-	// TODO : Lock pids list here
+	/* TODO : Lock pids list here */
 
-	// Iterate on children
+	/* Iterate on children */
 	struct task_struct *child;
+
 	list_for_each_entry(child, &current->children, sibling) {
 
-		// Extract pid of child
+		/* Extract pid of child */
 		pid_t child_id;
 		child_id = child->pid;
 
 		printk(KERN_DEBUG "Looking at child pid %zu\n", child_id);
 
-		// Increment number of children
+		/* Increment number of children */
 		children = children + 1;
 
 		if (children <= limit) {
-			// Store children pid in the list
-			if(put_user(child_id, list) == -EFAULT){
+			/* Store children pid in the list */
+			if (put_user(child_id, list) == -EFAULT) {
 				printk(KERN_ERR "Error while writing children id\n");
 				return -EFAULT;
 			}
-			// Moves pointer to next memory slot
+			/* Moves pointer to next memory slot */
 			list++;
 		}
 	}
 
-	if (children <= limit) {
-		// Need to return -ENOBUFFS;
+	if (children <= limit)
+		/* Need to return -ENOBUFFS; */
 		res = -ENOBUFS;
-	}
 
-	// Write the number of children in num_children
-	if(put_user(children, num_children) == -EFAULT) {
+	/* Write the number of children in num_children */
+	if (put_user(children, num_children) == -EFAULT)
 		printk(KERN_ERR, "Cannot write number of children\n");
-	}
 
 	return res;
 }
