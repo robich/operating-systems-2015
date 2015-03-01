478ba718be3ff5074bf499cc613e4251c708e600
Julien Perrenoud <julien.perrenoud@epfl.ch>
2015-03-01 14:01:58 +0100

    Update get_unique_id.c
    Lock issues

--- assignment01/linux/kernel/get_unique_id.c
+++ assignment01/linux/kernel/get_unique_id.c
@@ -4,30 +4,20 @@
 #include<linux/init.h>
 #include<linux/spinlock.h>
 
-spinlock_t lock;
+#define DEFINE_SPINLOCK(uuid_lock)
 long id;
-
-static int __init hi(void)
-{
-	spin_lock_init(&lock);
-	id = 0;
-
-	return 0;
-}
-
-module_init(hi);
 
 asmlinkage long sys_get_unique_id(int *uuid)
 {
 	/* Lock */
-	spin_lock(&lock);
+	spin_lock(&uuid_lock);
 
 	/* Try to write the ID */
 	long res;
 
 	res = put_user(id, uuid);
 
-	spin_unlock(&lock);
+	spin_unlock(&uuid_lock);
 
 	if (res == -EFAULT) {
 		printk(KERN_ERR "Trying to access an illegal memory location !\n");
